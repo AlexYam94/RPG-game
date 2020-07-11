fb1e4f56d3ecb8ecddc4cbaa904cf620c8099030
rm-rf-rm-rf-rm-rf <yamminghin@gmail.com>
2020-07-11 19:35:14 +0800

    Add cinematic trigger

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Saving;
+
+namespace RPG.cinematics
+{
+    public class CinematicTrigger : MonoBehaviour, ISaveable
+    {
+        private bool isTriggered = false;
+
+        // Start is called before the first frame update
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!isTriggered && other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                isTriggered = true;
+            }
+        }
+
+        public object CaptureState()
+        {
+            return isTriggered;
+        }
+
+        public void RestoreState(object state)
+        {
+            isTriggered = (bool)state;
+        }
+    }
+}
+
