337fc037f0887f6dbcd4e57e1b076ac539fa65bc
AlexYam <yamminghin@gmail.com>
2021-01-07 23:04:27 +0800

    change player health bar to slider

--- Assets/Scripts/Attributes/HealthBarDisplay.cs
+++ Assets/Scripts/Attributes/HealthBarDisplay.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RPG.Attributes
 {
@@ -10,10 +11,12 @@ namespace RPG.Attributes
         Health health;
         RectTransform rt;
         float barWidth = 250;
+        Slider slider = null;
 
         private void Awake()
         {
             rt = GetComponent<RectTransform>();
+            slider = GetComponent<Slider>();
         }
 
         // Start is called before the first frame update
@@ -24,9 +27,10 @@ namespace RPG.Attributes
         }
 
         void Update(){
-            Vector2 size = rt.sizeDelta;
-            size.x = barWidth * health.GetPercentage()/100;
-            rt.sizeDelta = size;
+            // Vector2 size = rt.sizeDelta;
+            // size.x = barWidth * health.GetPercentage()/100;
+            // rt.sizeDelta = size;
+            slider.value = slider.maxValue * health.GetFraction();
         }
     }
 }
