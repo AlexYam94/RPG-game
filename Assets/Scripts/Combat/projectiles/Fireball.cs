68f5d1d2150fb0b646290ba60148a98c01e1f435
AlexYam <yamminghin@gmail.com>
2021-01-08 20:43:15 +0800

    Add fireball script and burn damage

---
+++ Assets/Scripts/Combat/projectiles/Fireball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Attributes;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+
+    public class Fireball : MonoBehaviour
+    {
+        [SerializeField] float totalDamage = 3;
+        [SerializeField] float duration = 3;
+
+        private Health target = null;
+        GameObject instigator = null;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            SetTarget();
+        }
+
+        public void SetTarget()
+        {
+            this.target = GetComponent<Projectile>().GetTarget();
+            this.instigator = GetComponent<Projectile>().GetInstigator();
+        }
+
+        public void Burn()
+        {
+            if (target == null) return;
+            target.StartDamageOfTime(totalDamage, duration, instigator);
+        }
+    }
+
+}
