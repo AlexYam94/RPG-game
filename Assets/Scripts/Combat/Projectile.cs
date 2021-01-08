e0e19b32903e425825a79763f09c63111681450b
AlexYam <yamminghin@gmail.com>
2021-01-09 00:37:06 +0800

    Add getTarget and get Instigator to projectile script

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,18 +9,12 @@ namespace RPG.Combat
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField]
-        float speed = 1f;
-        [SerializeField]
-        bool isHoming = false;
-        [SerializeField]
-        GameObject hitEffect = null;
-        [SerializeField]
-        float maxLifeTime = 10f;
-        [SerializeField]
-        GameObject[] destroyOnHit = null;
-        [SerializeField]
-        float lifeAfterImpact = 1;
+        [SerializeField] float speed = 1f;
+        [SerializeField] bool isHoming = false;
+        [SerializeField] GameObject hitEffect = null;
+        [SerializeField] float maxLifeTime = 10f;
+        [SerializeField] GameObject[] destroyOnHit = null;
+        [SerializeField] float lifeAfterImpact = 1;
         [SerializeField] UnityEvent onHit;
 
         Health target = null;
@@ -49,6 +43,7 @@ namespace RPG.Combat
             Health health = other.gameObject.GetComponent<Health>();
 
             if (health != target) return;
+            print(target.gameObject.name);
 
             onHit.Invoke();
             if (hitEffect != null)
@@ -81,6 +76,14 @@ namespace RPG.Combat
             if (targetCapsule == null) return target.transform.position;
             return target.transform.position + Vector3.up * targetCapsule.height / 2;
             // return Vector3.forward * Time.deltaTime * speed;
+        }
+
+        public Health GetTarget(){
+            return target;
+        }
+
+        public GameObject GetInstigator() {
+            return instigator;
         }
     }
 
