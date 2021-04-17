0852cbfe626b12f98d31283bbafe4e2d31eba425
AlexYam <yamminghin@gmail.com>
2021-04-17 22:55:38 +0800

    add pickup manager

---
+++ Assets/Scripts/Controller/PickupManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameDevTV.Inventories;
+
+namespace RPG.Control
+{
+
+    public class PickupManager : MonoBehaviour
+    {
+        public static PickupManager instance;
+
+        private static List<Pickup> pickups = new List<Pickup>();
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = new PickupManager();
+            }
+        }
+
+        public static void Register(Pickup pickup)
+        {
+            pickups.Add(pickup);
+        }
+
+        public static void Deregister(Pickup pickup)
+        {
+            pickups.Remove(pickup);
+        }
+
+        public static void Pickup(){
+            if(pickups.Count==0||pickups[0]==null) return;
+            pickups[0].PickupItem();
+            Deregister(pickups[0]);
+        }
+    }
+}
