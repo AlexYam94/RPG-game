d533f0b6884141ffeb3ea758e5f324a78d5eb6b9
AlexYam <yamminghin@gmail.com>
2021-04-17 22:54:35 +0800

    add pick up button

---
+++ Assets/Scripts/Controller/ButtonPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Inventories;
+using UnityEngine;
+
+namespace RPG.Control
+{
+
+    public class ButtonPickup : MonoBehaviour
+    {
+        [SerializeField] GameObject uiContainer = null;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                uiContainer.SetActive(true);
+                //register this pickup uid? to pickup manager
+                PickupManager.Register(GetComponent<Pickup>());
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                uiContainer.SetActive(false);
+                //deregister this pickup uid? to pickup manager
+                PickupManager.Deregister(GetComponent<Pickup>());
+            }
+        }
+    }
+
+}
