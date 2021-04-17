6507fe2423694844be0b2359bfb3192b0f7027ff
AlexYam <yamminghin@gmail.com>
2021-04-17 18:15:50 +0800

    update exp bar

--- Assets/Scripts/Attributes/ExpBarDisplay.cs
+++ Assets/Scripts/Attributes/ExpBarDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using RPG.Attributes;
 using RPG.Stats;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExpBarDisplay : MonoBehaviour
 {
@@ -11,9 +12,11 @@ public class ExpBarDisplay : MonoBehaviour
     Experience experience = null;
     float currentExp = 0;
     float expToLevelUp = 0;
+    Slider slider = null;
 
     private void Awake()
     {
+        slider = GetComponent<Slider>();
         experience = GameObject.FindGameObjectWithTag("Player").GetComponent<Experience>();
         rt = GetComponent<RectTransform>();
         barWidth = rt.sizeDelta.x;
@@ -26,17 +29,22 @@ public class ExpBarDisplay : MonoBehaviour
     public void UpdateExpBar(float experiencePoint)
     {
         
-        Vector2 size = rt.sizeDelta;
+        // Vector2 size = rt.sizeDelta;
         currentExp += experiencePoint;
-        size.x = barWidth + (currentExp/expToLevelUp * 100);
-        if(size.x >= 100){
+        // size.x = barWidth + (currentExp/expToLevelUp * 100);
+        // if(size.x >= 100){
+        //     currentExp = 0;
+        //     size.x %= 100;
+        //     expToLevelUp = GetBarMaxNum();
+        // }
+        // rt.sizeDelta = size;
+        print(currentExp/expToLevelUp);
+        slider.value = slider.value + (currentExp/expToLevelUp);
+        if(slider.value >= 1){
             currentExp = 0;
-            size.x %= 100;
+            slider.value = 0;
             expToLevelUp = GetBarMaxNum();
         }
-        // print("expToLevelUp: " + expToLevelUp);
-        // print("currentExp: " + currentExp);
-        rt.sizeDelta = size;
     }
 
     private float GetBarMaxNum()
