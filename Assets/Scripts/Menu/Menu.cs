c11d1f60d29173e4fa64f34a8a03055eae827707
AlexYam <yamminghin@gmail.com>
2021-04-17 22:54:44 +0800

    add menu script

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] public Canvas menu = null;
+
+    private void Start() {
+        // Canvas menu = GetComponent<Canvas>();
+        // menu.gameObject.SetActive(false);
+    }
+
+    public void Resume(){
+        // menu.enabled=true;
+        GameObject.Find("Menu").GetComponent<Canvas>().enabled=false;
+    }
+    
+    public void OpenSettingMenu(){
+
+    }
+
+    public void Quit(){
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+}
