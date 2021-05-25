4db20e3fa85987ced144b4063f5de4029cbb4ce2
AlexYam <yamminghin@gmail.com>
2021-05-25 23:00:31 +0800

    Add Tests folder

---
+++ Assets/Tests/EditMode/StateMachineTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class StateMachineTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void StateMachineTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.AreEqual("test","test");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator StateMachineTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
