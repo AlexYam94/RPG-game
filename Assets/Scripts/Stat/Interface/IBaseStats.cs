f0e3bf9b240ae1b3aed3a0111c2742333fef9018
AlexYam <yamminghin@gmail.com>
2021-06-18 23:03:05 +0800

    Add interface to BaseStats for testing

---
+++ Assets/Scripts/Stat/Interface/IBaseStats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    public interface IBaseStats
+    {
+        event Action onLevelUp;
+
+        float GetStat(Stat stat);
+
+        float GetBaseStat(Stat stat);
+
+        int GetLevel();
+
+        int CalculateLevel();
+
+        float GetExpToLevelUp();
+    }
+}
